e323e0a96f1e1cd1972fd98c4f531495eaea12d9
temisan otokuefor <temipretty1992@gmail.com>
2019-08-07 11:41:06 +0100

    ft(user-can-register-for-a-course): this adds register course endpoints[Finishes #167589965]

--- server/v1/app/models/courses.js
+++ server/v1/app/models/courses.js
@@ -24,7 +24,7 @@ class Course {
     }
 
   /**
-   * @method getCoursess
+   * @method getCourses
    * @returns {object} All courses
    */
   static async getCourses() {
@@ -33,6 +33,49 @@ class Course {
     return result;
   }
 
+  /**
+   * @param {*} data
+   * @returns { object } user object
+   */
+  static addCourse(data) {
+    const queryText = `INSERT INTO userCourses (registeredOn, userId ,courseId)
+      VALUES ($1, $2, $3) RETURNING registeredOn, userId ,courseId;`;
+
+    const date =  moment(new Date());
+    const {
+      id,
+      user
+    } = data;
+
+    const values =  [date, user, id];
+    const response = db.query(queryText, values);
+    return response;
+  }
+
+  // /**
+  //  * @method regCourse
+  //  * @returns {object} registered courses
+  //  */
+  // static async regCourse() {
+  //   const queryText = `SELECT * FROM userCourses
+  //     JOIN courses ON userCourses.courseId = courses.id
+  //     WHERE id = $1;`;
+  //   const result = await db.query(queryText);
+  //   return result;
+  // }
+  /**
+   * @method getCourse
+   * @returns {object} A specific course
+   */
+  static async getCourse(userId, courseId) {
+    const queryText = `SELECT userCourses.courseId, courses.courseTitle,
+    courses.startDate, courses.endDate, users.email, userCourses.userId from userCourses
+    JOIN courses ON userCourses.courseId = courses.id
+    JOIN users ON userid = users.id
+    WHERE users.id = $1 AND courses.id = $2;`;
+    const result = db.query(queryText, [userId, courseId]);
+    return result;
+  }
 }
 export default Course;
   
--- server/v1/app/routes/courses.js
+++ server/v1/app/routes/courses.js
@@ -7,5 +7,6 @@ const courseRoutes = express.Router();
 
 courseRoutes.post('/courses', AuthenticateUser.verifyAdmin, InputValidator.validateCourse, CourseController.CreateCourse);
 courseRoutes.get('/courses', AuthenticateUser.verifyToken, CourseController.GetCourses);
+courseRoutes.post('/courses/:id/register', AuthenticateUser.verifyToken, CourseController.takeCouse);
 
 export default courseRoutes;
