658fc52161efb6412cb2a4496ce043a59be07118
temisan otokuefor <temipretty1992@gmail.com>
2020-10-05 21:54:19 +0100

    What does this PR do? - This adds server side pagination for courses
    Description of Task to be completed?
    - Adds pagination view endpoint to the course controller,
      filters all courses on each page and filters courses by course title
    
    How should this be manually tested?
    - Go to postman and make a Get request with user credentials on the
      endpoints below.
    ```
    http://localhost:3000/api/v1/courses/pagination
    
    http://localhost:3000/api/v1/courses/pagination?title=Training
    
    ```
    
    What are the relevant pivotal tracker stories?
    175132653

--- src/controllers/coursesControllers.js
+++ src/controllers/coursesControllers.js
@@ -100,5 +100,21 @@ class CourseController {
       return resErr(res, 400, error.message);
     }
   }
+
+  /**
+    * @method ViewPaginatedCourses
+    * @description Fetches all courses from the database
+    * @param {object} req - The Request Object
+    * @param {object} res - The Response Object
+    * @returns {object} JSON API Response
+    */
+  static async ViewPaginatedCourses(req, res) {
+    try {
+      const pageResult = await CourseServices.Pagination(req, res);
+      return resLong(res, 200, pageResult);
+    } catch (error) {
+      return resErr(res, 500, error.message);
+    }
+  }
 }
 export default CourseController;
--- src/services/courseServices.js
+++ src/services/courseServices.js
@@ -1,3 +1,4 @@
+import { Op } from 'sequelize';
 import { course, userCourses } from '../models';
 
 const Course = course;
@@ -84,5 +85,46 @@ export default class CourseServices {
   static async getAUserCourseBy(courseId, UserId) {
     const CourseDetails = await userCourses.findOne({ where: { courseId, UserId } });
     return CourseDetails;
+  }
+
+  /**
+   * @name Pagination
+   * @description Interacts with model to find a single Users Course
+   * @param { string } id the user id
+   * @returns {object} return the userCourses data
+   */
+  static async Pagination(req) {
+    const page = parseInt(req.query.page || 1, 10);
+    const nameSearch = req.query.title;
+    const limit = 6;
+
+    const offset = (page > 1) ? (page - 1) * limit : 0;
+    const filters = { limit, offset };
+    if (nameSearch) {
+      filters.where = {
+        courseTitle: {
+          [Op.iLike]: `%${nameSearch}%`,
+        },
+      };
+    }
+
+    return Course.findAndCountAll(filters)
+      .then((data) => {
+        const totalPages = Math.ceil(data.count / limit);
+        const response = {
+          message: `Paginating is completed! Query parameters: page = ${page}, limit = ${limit}`,
+          data: {
+            totalItems: data.count,
+            totalPages,
+            limit,
+            currentPageNumber: page + 1,
+            previousPageNumber: page - 1,
+            currentPageSize: data.rows.length,
+            courses: data.rows,
+          },
+        };
+
+        return response;
+      });
   }
 }
