825b3bf8d135d1c0d847ea1428f8f057fe5a7c3f
dewaleolaoye <adewaleolaoye21@gmail.com>
2020-10-14 18:22:01 +0100

    async and await test

--- src/test/authTest.js
+++ src/test/authTest.js
@@ -1,8 +1,8 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import faker from 'faker';
+// import faker from 'faker';
 
-import app from '../app';
+// import app from '../app';
 
 // const expect = chai.expect;
 
@@ -10,170 +10,170 @@ chai.should();
 
 chai.use(chaiHttp);
 
-const apiEndPoint = '/api/v1/';
-const userEndPoint = `${apiEndPoint}auth/`;
+// const apiEndPoint = '/api/v1/';
+// const userEndPoint = `${apiEndPoint}auth/`;
 
-const user = {
-  email: faker.internet.email(),
-  password: faker.internet.password(),
-};
+// const user = {
+//   email: faker.internet.email(),
+//   password: faker.internet.password(),
+// };
 
-const userWithNoEmail = {
-  password: '1234',
-  firstName: 'ade',
-  lastName: 'wale',
-};
+// const userWithNoEmail = {
+//   password: '1234',
+//   firstName: 'ade',
+//   lastName: 'wale',
+// };
 
-const userEmailWithNoKeyValue = {
-  email: '',
-  password: '1234',
-  firstName: 'ade',
-  lastName: 'wale',
-};
+// const userEmailWithNoKeyValue = {
+//   email: '',
+//   password: '1234',
+//   firstName: 'ade',
+//   lastName: 'wale',
+// };
 
-const userWithNoPassword = {
-  email: 'ad@gmail.com',
-  firstName: 'ade',
-  lastName: 'wale',
-};
+// const userWithNoPassword = {
+//   email: 'ad@gmail.com',
+//   firstName: 'ade',
+//   lastName: 'wale',
+// };
 
 describe('Signup User', () => {
-  it('Should create a new user', (done) => {
-    chai.request(app)
-      .post(`${userEndPoint}signup`)
-      .send(user)
-      .end((err, res) => {
-        res.should.have.status(201);
-        res.body.should.be.a('object');
-        res.body.should.have.property('data');
-        res.body.data.should.be.a('object');
-        res.body.data.should.have.property('tokens');
-        res.body.data.should.have.property('id');
-        res.body.data.should.have.property('email');
-        done();
-      });
-  });
-  it('Should return 409 if email already exists', (done) => {
-    chai.request(app)
-      .post(`${userEndPoint}signup`)
-      .send(user)
-      .end((err, res) => {
-        res.should.have.status(409);
-        res.body.should.be.a('object');
-        res.body.should.have.property('error');
-        done();
-      });
-  });
-  it('Should return 400 if password is omitted', (done) => {
-    chai
-      .request(app)
-      .post(`${userEndPoint}signup`)
-      .send(userWithNoPassword)
-      .end((err, res) => {
-        res.should.have.status(400);
-        res.body.should.be.a('object');
-        res.body.should.have.property('error');
-        done();
-      });
-  });
+  // it('Should create a new user', (done) => {
+  //   chai.request(app)
+  //     .post(`${userEndPoint}signup`)
+  //     .send(user)
+  //     .end((err, res) => {
+  //       res.should.have.status(201);
+  //       res.body.should.be.a('object');
+  //       res.body.should.have.property('data');
+  //       res.body.data.should.be.a('object');
+  //       res.body.data.should.have.property('tokens');
+  //       res.body.data.should.have.property('id');
+  //       res.body.data.should.have.property('email');
+  //       done();
+  //     });
+  // });
+//   it('Should return 409 if email already exists', (done) => {
+//     chai.request(app)
+//       .post(`${userEndPoint}signup`)
+//       .send(user)
+//       .end((err, res) => {
+//         res.should.have.status(409);
+//         res.body.should.be.a('object');
+//         res.body.should.have.property('error');
+//         done();
+//       });
+//   });
+//   it('Should return 400 if password is omitted', (done) => {
+//     chai
+//       .request(app)
+//       .post(`${userEndPoint}signup`)
+//       .send(userWithNoPassword)
+//       .end((err, res) => {
+//         res.should.have.status(400);
+//         res.body.should.be.a('object');
+//         res.body.should.have.property('error');
+//         done();
+//       });
+//   });
 
-  it('Should return 400 if email is omitted', (done) => {
-    chai
-      .request(app)
-      .post(`${userEndPoint}signup`)
-      .send(userWithNoEmail)
-      .end((err, res) => {
-        res.should.have.status(400);
-        res.body.should.be.a('object');
-        res.body.should.have.property('error');
-        done();
-      });
-  });
+//   it('Should return 400 if email is omitted', (done) => {
+//     chai
+//       .request(app)
+//       .post(`${userEndPoint}signup`)
+//       .send(userWithNoEmail)
+//       .end((err, res) => {
+//         res.should.have.status(400);
+//         res.body.should.be.a('object');
+//         res.body.should.have.property('error');
+//         done();
+//       });
+//   });
 
-  it('Should return 400 if email key is provided without value', (done) => {
-    chai
-      .request(app)
-      .post(`${userEndPoint}signup`)
-      .send(userEmailWithNoKeyValue)
-      .end((err, res) => {
-        res.should.have.status(400);
-        res.body.should.be.a('object');
-        res.body.should.have.property('error');
-        done();
-      });
-  });
-});
+//   it('Should return 400 if email key is provided without value', (done) => {
+//     chai
+//       .request(app)
+//       .post(`${userEndPoint}signup`)
+//       .send(userEmailWithNoKeyValue)
+//       .end((err, res) => {
+//         res.should.have.status(400);
+//         res.body.should.be.a('object');
+//         res.body.should.have.property('error');
+//         done();
+//       });
+//   });
+// });
 
-describe('Signin', () => {
-  it('Should login a user successfully', (done) => {
-    chai.request(app)
-      .post(`${userEndPoint}signin`)
-      .send(user)
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a('object');
-        res.body.should.have.property('data');
-        res.body.data.should.be.a('object');
-        res.body.data.should.have.property('tokens');
-        done();
-      });
-  });
-  it('Should deny access if wrong email is provided', (done) => {
-    const login = {
-      email: 'thisEmailDoesNotExist@gmail.com',
-      password: 'pA55w0rd',
-    };
-    chai.request(app)
-      .post(`${userEndPoint}signin`)
-      .send(login)
-      .end((err, res) => {
-        res.should.have.status(404);
-        res.body.should.be.a('object');
-        res.body.should.have.property('error');
-        done();
-      });
-  });
-  it('Should deny access if wrong password is provided', (done) => {
-    const login = {
-      email: user.email,
-      password: 'justarandominvalidpassword',
-    };
-    chai.request(app)
-      .post(`${userEndPoint}signin`)
-      .send(login)
-      .end((err, res) => {
-        res.should.have.status(401);
-        res.body.should.be.a('object');
-        res.body.should.have.property('error');
-        done();
-      });
-  });
-  it('Should return 400 if password is ommited', (done) => {
-    const login = {
-      email: user.email,
-    };
-    chai.request(app)
-      .post(`${userEndPoint}signin`)
-      .send(login)
-      .end((err, res) => {
-        res.should.have.status(400);
-        res.body.should.be.a('object');
-        res.body.should.have.property('error');
-        done();
-      });
-  });
-  it('Should return 400 if email is not provided', (done) => {
-    const login = {
-      password: 'password',
-    };
-    chai.request(app)
-      .post(`${userEndPoint}signin`)
-      .send(login)
-      .end((err, res) => {
-        res.should.have.status(400);
-        res.body.should.be.a('object');
-        res.body.should.have.property('error');
-        done();
-      });
-  });
+// describe('Signin', () => {
+//   it('Should login a user successfully', (done) => {
+//     chai.request(app)
+//       .post(`${userEndPoint}signin`)
+//       .send(user)
+//       .end((err, res) => {
+//         res.should.have.status(200);
+//         res.body.should.be.a('object');
+//         res.body.should.have.property('data');
+//         res.body.data.should.be.a('object');
+//         res.body.data.should.have.property('tokens');
+//         done();
+//       });
+//   });
+//   it('Should deny access if wrong email is provided', (done) => {
+//     const login = {
+//       email: 'thisEmailDoesNotExist@gmail.com',
+//       password: 'pA55w0rd',
+//     };
+//     chai.request(app)
+//       .post(`${userEndPoint}signin`)
+//       .send(login)
+//       .end((err, res) => {
+//         res.should.have.status(404);
+//         res.body.should.be.a('object');
+//         res.body.should.have.property('error');
+//         done();
+//       });
+//   });
+//   it('Should deny access if wrong password is provided', (done) => {
+//     const login = {
+//       email: user.email,
+//       password: 'justarandominvalidpassword',
+//     };
+//     chai.request(app)
+//       .post(`${userEndPoint}signin`)
+//       .send(login)
+//       .end((err, res) => {
+//         res.should.have.status(401);
+//         res.body.should.be.a('object');
+//         res.body.should.have.property('error');
+//         done();
+//       });
+//   });
+//   it('Should return 400 if password is ommited', (done) => {
+//     const login = {
+//       email: user.email,
+//     };
+//     chai.request(app)
+//       .post(`${userEndPoint}signin`)
+//       .send(login)
+//       .end((err, res) => {
+//         res.should.have.status(400);
+//         res.body.should.be.a('object');
+//         res.body.should.have.property('error');
+//         done();
+//       });
+//   });
+//   it('Should return 400 if email is not provided', (done) => {
+//     const login = {
+//       password: 'password',
+//     };
+//     chai.request(app)
+//       .post(`${userEndPoint}signin`)
+//       .send(login)
+//       .end((err, res) => {
+//         res.should.have.status(400);
+//         res.body.should.be.a('object');
+//         res.body.should.have.property('error');
+//         done();
+//       });
+//   });
 });
---
+++ src/test/fixTest.js
@@ -0,0 +1,53 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import faker from 'faker';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import app from '../app';
+
+chai.use(chaiHttp);
+chai.use(sinonChai);
+
+const { expect } = chai;
+
+let request = chai.request(app).keepOpen();
+// let token;
+
+const apiEndPoint = '/api/v1/';
+const userEndPoint = `${apiEndPoint}auth/`;
+
+const userData = {
+  email: faker.internet.email(),
+  password: faker.internet.password(),
+};
+
+const userWithNoPassword = {
+  email: 'ad@gmail.com',
+};
+
+
+describe('AUTH', () => {
+  before(async () => {
+    request = chai.request(app).keepOpen();
+  });
+
+  afterEach(async () => {
+    sinon.restore();
+  });
+
+  describe('SignUp a user', () => {
+    it('should sign up user', async () => {
+      const response = await request.post(`${userEndPoint}signup`).send(userData);
+      expect(response.status).to.equal(201);
+      expect(response.body).to.be.a('object');
+    }).timeout(0);
+    it('Should return 400 Bad Request', async () => {
+      const response = await request.post(`${userEndPoint}signup`).send(userWithNoPassword);
+      expect(response.status).to.equal(400);
+    }).timeout(0);
+    it('should throw USER already exist error', async () => {
+      const response = await request.post(`${userEndPoint}signup`).send(userData);
+      expect(response.status).to.equal(409);
+    }).timeout(0);
+  });
+});
